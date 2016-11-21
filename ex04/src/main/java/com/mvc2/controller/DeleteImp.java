db490ebf0cfba6a2c0eeb04933f0b7555ca0959c
HB6017\user <chkmkm458.gmail.com>
2016-11-21 11:58:10 +0900

    수정페이지 추가2

---
+++ ex04/src/main/java/com/mvc2/controller/DeleteImp.java
@@ -0,0 +1,14 @@
+package com.mvc2.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeleteImp implements InterController {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse res) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
